b0e3c2024ededb3b8b3fe255a34729286e93d250
mrukhlov <mrukhlov@speaktoit.com>
2016-11-23 12:48:07 +0300

    test update

--- app.py
+++ app.py
@@ -29,10 +29,15 @@ def index_getter(letter):
 def webhook():
 	req = request.get_json(silent=True, force=True)
 
-	action = req['result']['action']
-	res = {'test':'test'}
+	speech = None
+	if req['result']["fulfillment"].has_key('speech'):
+		speech = req["result"]["fulfillment"]['speech']
 
-	if action == 'game.correct_guess' or action == 'game.wrong_guess':
+	action = None
+	if req['result'].has_key('action'):
+		action = req['result']['action']
+
+	'''if action == 'game.correct_guess' or action == 'game.wrong_guess':
 		res = CheckLetter(req)
 	elif action == 'game.correct.word':
 		res = correctWord(req)
@@ -41,7 +46,32 @@ def webhook():
 	else:
 		log.error("Unexpeted action.")
 
-	return make_response(jsonify(res))
+	return make_response(jsonify(res))'''
+
+	if action == 'game.correct_guess' or action == 'game.wrong_guess':
+		res, picture = CheckLetter(req)
+	elif action == 'game.correct.word':
+		guess_word = ['K', '_', '_', '_', '_', 'N']
+		res = speech
+	elif action == 'confirmation.yes':
+		res = speech.replace(' _ _ _ _ ', '____')
+	elif action == 'game.start':
+		guess_word = ['K', '_', '_', '_', '_', 'N']
+		img_links = [
+			'https://s22.postimg.org/r9eee4w4d/image.png',
+			'https://s22.postimg.org/z3jld9tb1/image.png',
+			"https://s22.postimg.org/ih21470d9/image.png"
+		]
+		res = speech
+	elif action and action.startswith('smalltalk'):
+		res = speech
+	elif action == 'slack_test':
+		slack_message = {'text':'aaa', "attachments": [{"title": "IMAGE", "image_url": "https://s22.postimg.org/ih21470d9/image.png"}]}
+		return {"data": {"slack": {slack_message}}}
+	else:
+		res = None
+
+	return res, action, picture
 
 def gameReset(req):
 	guess_word = ['K', '_', '_', '_', '_', 'N']
@@ -59,18 +89,24 @@ def CheckLetter(req):
 	letter_index = index_getter(letter)
 	letter_diff = [i for i in list('kitten'.upper()) if i not in guess_word]
 
-	print guess_word.count('_') - 1
-	if guess_word.count('_') - 1 > 0:
+	picture = True
+
+	if guess_word.count('_') - 1 > -1:
 		if guess_word.count(letter) == 0 and letter in letter_diff:
 			for i in letter_index:
 				guess_word[i] = letter
-			output = "That's right! " + ''.join(guess_word) + '. Guess the next one.'
-		elif letter not in letter_diff:
-			output = 'Almost there. Try again!'
+			if '_' in guess_word:
+				output = "That's right! " + ''.join(guess_word) + '. Guess the next one.'
+			else:
+				output = 'You are so smart! Fantastic! Here is your kitten.'
+				global guess_word
+				global img_links
+				guess_word = ['K', '_', '_', '_', '_', 'N']
+		elif letter not in letter_diff and letter in guess_word:
+			output = 'You have already guessed this letter. Try again.'
+			picture = False
 		else:
-			output = 'You named it already, try again. Word is ' + ''.join(guess_word)
-	else:
-		output = 'You are so smart! Fantastic! Here is your kitten.'
+			output = 'Almost there. Try again!'
 
 	return {
 		"speech": output,
@@ -86,8 +122,6 @@ def correctWord(req):
 		"displayText": output,
 		"contextOut": []
 	}
-
-
 
 if __name__ == '__main__':
 	port = int(os.getenv('PORT', 5000))
