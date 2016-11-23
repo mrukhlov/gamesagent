46b462d83b93e1f0fb9f78bc0a66f59646e680d3
mrukhlov <mrukhlov@speaktoit.com>
2016-11-23 14:26:47 +0300

    test update

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import os
-import random
 
 from flask import (
 	Flask,
@@ -13,7 +12,8 @@ from flask import (
 app = Flask(__name__)
 log = app.logger
 
-global guess_word
+#global guess_word
+#global img_links
 guess_word = ['K', '_', '_', '_', '_', 'N']
 
 def index_getter(letter):
@@ -27,6 +27,7 @@ def index_getter(letter):
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
+
 	req = request.get_json(silent=True, force=True)
 
 	speech = None
@@ -37,34 +38,16 @@ def webhook():
 	if req['result'].has_key('action'):
 		action = req['result']['action']
 
-	'''if action == 'game.correct_guess' or action == 'game.wrong_guess':
-		res = CheckLetter(req)
+	if action == 'game.correct_guess' or action == 'game.wrong_guess':
+		res = checkLetter(req)
 	elif action == 'game.correct.word':
 		res = correctWord(req)
 	elif action == 'confirmation.yes':
 		res = gameReset(req)
-	else:
-		log.error("Unexpeted action.")
-
-	return make_response(jsonify(res))'''
-
-	if action == 'game.correct_guess' or action == 'game.wrong_guess':
-		res, picture = CheckLetter(req)
-	elif action == 'game.correct.word':
-		guess_word = ['K', '_', '_', '_', '_', 'N']
-		res = speech
-	elif action == 'confirmation.yes':
-		res = speech.replace(' _ _ _ _ ', '____')
 	elif action == 'game.start':
-		guess_word = ['K', '_', '_', '_', '_', 'N']
-		img_links = [
-			'https://s22.postimg.org/r9eee4w4d/image.png',
-			'https://s22.postimg.org/z3jld9tb1/image.png',
-			"https://s22.postimg.org/ih21470d9/image.png"
-		]
-		res = speech
+		res = gameStart(req)
 	elif action and action.startswith('smalltalk'):
-		res = speech
+		res = smallTalk(req)
 	elif action == 'slack_test':
 		slack_message = {'text':'aaa', "attachments": [{"title": "IMAGE", "image_url": "https://s22.postimg.org/ih21470d9/image.png"}]}
 
@@ -72,25 +55,65 @@ def webhook():
 	else:
 		res = None
 
-	return res, action, picture
+	return make_response(jsonify(res))
 
-def gameReset(req):
+def gameStart(req):
+
+	global guess_word
+	global img_links
+
 	guess_word = ['K', '_', '_', '_', '_', 'N']
-	output = "Cool! Let's start. The word is K _ _ _ _ N. Guess a letter! Note that every time you guess a letter, your cat is growing."
+	img_links = [
+		'https://s22.postimg.org/r9eee4w4d/image.png',
+		'https://s22.postimg.org/z3jld9tb1/image.png',
+		"https://s22.postimg.org/ih21470d9/image.png"
+	]
+
+	speech = None
+	if req['result']["fulfillment"].has_key('speech'):
+		speech = req["result"]["fulfillment"]['speech']
 
 	return {
-		"speech": output,
-		"displayText": output,
+		"speech": speech,
+		"displayText": speech
+	}
+
+def smallTalk(req):
+
+	speech = None
+	if req['result']["fulfillment"].has_key('speech'):
+		speech = req["result"]["fulfillment"]['speech']
+
+	return {
+		"speech": speech,
+		"displayText": speech
+	}
+
+def gameReset(req):
+
+	speech = None
+	if req['result']["fulfillment"].has_key('speech'):
+		speech = req["result"]["fulfillment"]['speech'].replace(' _ _ _ _ ', '____')
+
+	guess_word = ['K', '_', '_', '_', '_', 'N']
+
+	return {
+		"speech": speech,
+		"displayText": speech,
 		"contextOut": ['yes']
 	}
 
-def CheckLetter(req):
+def checkLetter(req):
+
+	speech = None
+	if req['result']["fulfillment"].has_key('speech'):
+		speech = req["result"]["fulfillment"]['speech']
+
+	attachments = []
 
 	letter = req['result']['resolvedQuery'].upper()
 	letter_index = index_getter(letter)
 	letter_diff = [i for i in list('kitten'.upper()) if i not in guess_word]
-
-	picture = True
 
 	if guess_word.count('_') - 1 > -1:
 		if guess_word.count(letter) == 0 and letter in letter_diff:
@@ -98,30 +121,64 @@ def CheckLetter(req):
 				guess_word[i] = letter
 			if '_' in guess_word:
 				output = "That's right! " + ''.join(guess_word) + '. Guess the next one.'
+
+				if len(img_links) > 0:
+					link = img_links[0]
+					img_links.remove(link)
+
+				slack_message = {
+					'text': output, "attachments":
+					[
+						{"title": "IMAGE", "image_url": link}
+					]
+			     }
 			else:
 				output = 'You are so smart! Fantastic! Here is your kitten.'
+
+				slack_message = {
+					'text': output, "attachments":
+					[
+						{"title": "IMAGE", "image_url": "https://s22.postimg.org/ih21470d9/image.png"}
+					]
+			     }
+
 				global guess_word
 				global img_links
 				guess_word = ['K', '_', '_', '_', '_', 'N']
 		elif letter not in letter_diff and letter in guess_word:
 			output = 'You have already guessed this letter. Try again.'
-			picture = False
+			slack_message = {'text': output}
 		else:
 			output = 'Almost there. Try again!'
+			slack_message = {'text': output}
 
 	return {
-		"speech": output,
+		#"speech": output,
 		"displayText": output,
-		"contextOut": []
+		"contextOut": [],
+		"data": {"slack": slack_message}
 	}
 
 def correctWord(req):
-	output = 'You are so smart! Fantastic! Here is your kitten.'
+
+	speech = None
+	if req['result']["fulfillment"].has_key('speech'):
+		speech = req["result"]["fulfillment"]['speech']
+
+	slack_message = {
+		'text': speech, "attachments":
+		[
+			{"title": "IMAGE", "image_url": "https://s22.postimg.org/ih21470d9/image.png"}
+		]
+     }
+
 	guess_word = ['K', '_', '_', '_', '_', 'N']
+
 	return {
-		"speech": output,
-		"displayText": output,
-		"contextOut": []
+		"speech": speech,
+		"displayText": speech,
+		"contextOut": [],
+		"data": {"slack": slack_message}
 	}
 
 if __name__ == '__main__':
